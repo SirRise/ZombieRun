8249e492b3aca804bdc2c90010aac17252350a0d
Gordon Fassbender <gordon.fassbender@gmx.de>
2017-12-10 21:04:51 +0100

    first commit

---
+++ js/entities/runner.js
@@ -0,0 +1,5 @@
+export default class Runner {
+    init() {
+        context.fill(0, 0, 50, 50);
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,6 @@
+import Runner from 'entities/runner.js';
+
+canvas = document.getElementById('screen');
+context = canvas.getContext('2d');
+
+context.fillRect(0, 0, canvas.width, canvas.height)
